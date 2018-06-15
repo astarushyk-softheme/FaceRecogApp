ea2777f37037c9a2ee7f4424f2966780ca609d9b
Artem Starushyk <artem.starushyk@softheme.com>
2018-06-15 17:59:51 +0300

    Simple test.

--- FaceRecog.App/FaceRecog.Web.API/Controllers/ValuesController.cs
+++ FaceRecog.App/FaceRecog.Web.API/Controllers/ValuesController.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using FaceRecog.Web.API.Services.FaceRecognition;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FaceRecog.Web.API.Controllers
@@ -6,10 +8,17 @@ namespace FaceRecog.Web.API.Controllers
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
+        private readonly IFaceRecognitionClient _client;
+        public ValuesController(IFaceRecognitionClient client)
+        {
+            _client = client;
+        }
+
         // GET api/values
         [HttpGet]
-        public IEnumerable<string> Get()
+        public async Task<IEnumerable<string>> Get()
         {
+            await _client.Test();
             return new string[] { "value1", "value2" };
         }
 
--- FaceRecog.App/FaceRecog.Web.API/Services/FaceRecognition/FaceRecognitionClient.cs
+++ FaceRecog.App/FaceRecog.Web.API/Services/FaceRecognition/FaceRecognitionClient.cs
@@ -1,4 +1,7 @@
-﻿using FaceRecog.Domain.Settings;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using FaceRecog.Domain.Settings;
 using Microsoft.Extensions.Options;
 using Microsoft.ProjectOxford.Face;
 
@@ -12,5 +15,32 @@ namespace FaceRecog.Web.API.Services.FaceRecognition
         {
             _client = new FaceServiceClient(settings.Value.SubscriptionKey, settings.Value.ApiRoot);
         }
+
+        public async Task Test()
+        {
+            var filePath = @"D:\170705133020_1_540x360.jpg";
+
+            IEnumerable<FaceAttributeType> faceAttributes =
+                new []
+                {
+                    FaceAttributeType.Gender,
+                    FaceAttributeType.Age,
+                    FaceAttributeType.Smile,
+                    FaceAttributeType.Glasses,
+                    FaceAttributeType.FacialHair,
+                    FaceAttributeType.HeadPose
+                };
+
+            try
+            {
+                using (Stream imageFileStream = File.OpenRead(filePath))
+                {
+                    var faces = await _client.DetectAsync(imageFileStream, returnFaceId: true, returnFaceLandmarks: false, returnFaceAttributes: faceAttributes);
+                }
+            }
+            catch
+            { }
+        }
+
     }
 }
